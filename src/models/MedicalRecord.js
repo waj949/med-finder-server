88b117c99c02a278e6ca501b7e71e8651bb2a782
Mohamed-Dhia <dhiaweslati16@gmail.com>
2020-01-28 17:42:03 +0100

    add validator dep created Doctor, MedicalRecord, Medicine and patient

---
+++ src/models/MedicalRecord.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const MedicalRecord = new mongoose.Schema(
+  {
+    patient: { type: Schema.Types.ObjectId, ref: "Patient", required: true },
+
+    problems: String, //should be revised : structure wise
+    medications: String, //should be revised : structure wise
+    psychiatric: String, //should be revised : structure wise
+    allergirs: String //should be revised : structure wise
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("MedicalRecord", MedicalRecord);
---
+++ src/models/Patient.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const Patient = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Please enter a valid first name"]
+    },
+
+    lastName: {
+      type: String,
+      required: [true, "Please enter a valid last name"]
+    },
+
+    password: { type: String, required: true },
+
+    phoneNumber: String, // should it be required ?
+
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      validate: value => {
+        if (!validator.isEmail(value))
+          throw new Error({ error: "Invalid Email adress" });
+      }
+    },
+
+    adress: String, //should be revised
+
+    image: String, //should be revised
+
+    birthDate: String,
+
+    gender: String, //we can add a validator here
+
+    geoLocation: String, // why does he need it ?
+
+    medicalRecord: {
+      type: Schema.Types.ObjectId,
+      ref: "MedicalRecord",
+      required: true
+    },
+    doctor: {
+      type: Schema.Types.ObjectId,
+      ref: "MedicalRecord",
+      required: true
+    } // should we have this
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Patient", Patient);
