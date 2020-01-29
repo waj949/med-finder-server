7e16e43013f0ed2f31ddc64f63f3e7a58d250282
waj949 <wajdihamrouni1993@gmail.com>
2020-01-29 19:31:10 +0100

    add longitude and latitude to schemas

--- src/services/pharmacyServices.js
+++ src/services/pharmacyServices.js
@@ -9,14 +9,15 @@ export default class PharmacyServices {
             const newPharmacy = new Pharmacy({
               'name': newPharmacy.name,
               'adress' :newPharmacy.adress,
-              'contact':newPharmacy.contact,
-              'geoLocation':newPharmacy.geoLocation,
+              'phoneNumber':newPharmacy.contact,
+              'latitude':newPharmacy.latitude,
+              'longitude':newPharmacy.longitude,
               'openingHours':newPharmacy.openingHours,
               'feedbacks':newPharmacy.feedbacks
             });
             console.log('before save');
-            let saveUser = await newUser.save(); //when fail its goes to catch
-            console.log(saveUser); //when success it print.
+            let savedPharmacy = await newPharmacy.save(); //when fail its goes to catch
+            console.log(newPharmacy); //when success it print.
             console.log('after save');
           } catch (err) {
             console.log('err' + err);
