282c67cca54d0607751df3efb9b52f3fbf26da6e
Mohamed-Dhia <dhiaweslati16@gmail.com>
2020-01-31 17:57:05 +0100

    didnt commit in a while

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -8,11 +8,11 @@ export default ({ app }) => {
   app.get("/status", (req, res) => {
     res.status(200).end();
   });
-  
+
   app.head("/status", (req, res) => {
     res.status(200).end();
   });
-  
+
   app.enable("trust proxy");
 
   app.use(cors());
--- src/services/pharmacyServices.js
+++ src/services/pharmacyServices.js
@@ -1,38 +1,37 @@
-import Pharmacy from '../models/Pharmacy'
-var pharmacy = new Pharmacy()
+import Pharmacy from "../models/Pharmacy";
+// var pharmacy = new Pharmacy()
 export default class PharmacyServices {
-    constructor(pharmacy){
-      pharmacy = new Pharmacy()
+  constructor(pharmacy) {
+    pharmacy = new Pharmacy();
+  }
+  async createPharmacy(pharmacy) {
+    try {
+      const newPharmacy = new Pharmacy({
+        name: pharmacy.name,
+        adress: pharmacy.adress,
+        phoneNumber: pharmacy.phoneNumber,
+        latitude: pharmacy.latitude,
+        longitude: pharmacy.longitude,
+        openingHour: pharmacy.openingHour,
+        closingHour: pharmacy.closingHour,
+        feedbacks: pharmacy.feedbacks,
+        password: pharmacy.password
+      });
+
+      let savedPharmacy = await newPharmacy.save(); //when fail its goes to catch
+      console.log("pharmacy created in database in database"); //when successsss it print.
+      return savedPharmacy;
+    } catch (err) {
+      console.log("err" + err);
+      res.status(500).send(err);
     }
-    async createPharmacy(pharmacy){
-        try {
-            const newPharmacy = new Pharmacy({
-              "name": pharmacy.name,
-              "adress": pharmacy.adress,
-              "phoneNumber": pharmacy.phoneNumber,
-              "latitude": pharmacy.latitude,
-              "longitude": pharmacy.longitude,
-              "openingHour": pharmacy.openingHour,
-              "closingHour": pharmacy.closingHour,
-              "feedbacks": pharmacy.feedbacks,
-              "password": pharmacy.password
-          });
-            
-            let savedPharmacy = await newPharmacy.save(); //when fail its goes to catch
-            console.log("pharmacy created in database in database"); //when successsss it print.
-            return savedPharmacy
-          } catch (err) {
-            console.log('err' + err);
-            res.status(500).send(err);
-          }
-    }
-   async locatePharmacies(){
-    var found = await  Pharmacy.find({})
+  }
+  async locatePharmacies() {
+    var found = await Pharmacy.find({});
     return found;
-   }
-   async searchPharmacies(query){
-    var searchResult = await Pharmacy.search(query)
+  }
+  async searchPharmacies(query) {
+    var searchResult = await Pharmacy.search(query);
     return searchResult;
-    }
-   
+  }
 }
