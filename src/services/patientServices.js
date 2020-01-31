ac5f3decdc1a762a55a2d84666719a289582afdb
waj949 <wajdihamrouni1993@gmail.com>
2020-01-31 15:09:08 +0100

    patientServices

---
+++ src/services/patientServices.js
@@ -0,0 +1,30 @@
+import Patient from '../models/Patient'
+var patient = new Patient()
+export default class PatientServices {
+    constructor(patient){
+        patient = new Patient()
+    }
+    async createPatient(patient){
+        try {
+            const newPatient = new Patient({
+              "firstName": patient.firstName,
+              "lastName": patient.lastName,
+              "phoneNumber": patient.phoneNumber,
+              "email": patient.email,
+              "password": patient.password,
+              "adress": patient.adress,
+              "image": patient.image,
+              "birthDate": patient.birthDate,
+              "gender": patient.gender
+          });
+            
+            let savedPatient = await newPatient.save(); //when fail its goes to catch
+            console.log("patient created in database "); //when successsss it print.
+            return savedPatient;
+          } catch (err) {
+            console.log('err' + err);
+            res.status(500).send(err);
+          }
+    }
+   
+}
